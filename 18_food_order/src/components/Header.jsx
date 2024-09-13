6fe6eb0b8afee271df24b7aae719fef8770a8e54
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-09-13 20:42:32 +0800

    feat(food-order): cart modal

--- 18_food_order/src/components/Header.jsx
+++ 18_food_order/src/components/Header.jsx
@@ -2,13 +2,21 @@ import logoImg from "../assets/logo.jpg";
 import Button from "./ui/Button";
 import { useContext } from "react";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
   const totalCartItems = cartCtx.items.reduce(
     (totalNumberOfItems, item) => totalNumberOfItems + item.quantity,
     0
   );
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
     <header id="main-header">
       <div id="title">
@@ -16,7 +24,9 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly> Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
